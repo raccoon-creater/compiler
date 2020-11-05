d58c6bc1cc2caeb02a71d88d0710f4809eea80fd
raccoon-creater <secracc460192@gmail.com>
2020-11-05 13:24:29 +0000

    トークナイザを導入

--- CalculatorLevel/9cc.c
+++ CalculatorLevel/9cc.c
@@ -5,22 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-//トークンの種類
-typedef enum{
-  TK_RESERVED,  //記号
-  TK_NUM,       //整数トークン
-  TK_EOF,       //入力の終わりを表すトークン
+// トークンの種類
+typedef enum {
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
 } TokenKind;
 
 typedef struct Token Token;
 
-//トークン型
-struct Token{
-  TokenKind kind; //トークンの型
-  Token *next;    //次の入力トークン
-  int val;        //kindがTK_NUM
-  char *str;      //トークン文字列
-}
+// トークン型
+struct Token {
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力トークン
+  int val;        // kindがTK_NUMの場合、その数値
+  char *str;      // トークン文字列
+};
 
 //現在着目しているトークン
 Token *token;
