b00117ce74060830c38af341896f435a0ed2a91f
raccoon-creater <secracc460192@gmail.com>
2020-11-07 08:06:39 +0000

    単項プラスと単項マイナス(仮)

--- CalculatorLevel/9cc.c
+++ CalculatorLevel/9cc.c
@@ -167,6 +167,7 @@ Node *new_node_num(int val){
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr(){
@@ -182,19 +183,29 @@ Node *expr(){
         }
     }
 }
-
+//mul = unary("*",unary | "/" unary)*
 Node *mul(){
-    Node *node = primary();
+    Node *node = unary();
 
     for(;;){
         if(consume('*')){
-            node = new_node(ND_MUL,node,primary());
+            node = new_node(ND_MUL,node,unary());
         }else if(consume('/')){
-            node = new_node(ND_DIV,node,primary());
+            node = new_node(ND_DIV,node,unary());
         }else{
             return node;
         }
     }
+}
+
+Node *unary(){
+  if(consume('+')){
+    return primary();
+  }
+  if(consume('-')){
+    return new_node(ND_SUB,new_node_num(0),primary());
+  }
+  return primary();
 }
 
 Node *primary(){
